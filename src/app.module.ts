b9bdacf672383729ce1c766b4b0f7cbb57b98fa0
Mariana Bandeira <marianabandeira.cicm@gmail.com>
2025-12-17 21:47:02 -0300

    Atualizações no projeto

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Postagem } from './postagem/entities/postagem.entity';
 import { PostagemModule } from './postagem/postagem.module';
 import { TemaModule } from './tema/tema.module';
-import { Tema } from './tema/entities/tema.entity';
 import { AuthModule } from './auth/auth.module';
-import { Usuario } from './usuario/entities/usuario.entity';
 import { UsuarioModule } from './usuario/usuario.module';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
